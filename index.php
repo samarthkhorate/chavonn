46cc86ce8ccccacaebe93660bd940b971ef00cb9
samarthkhorate <samkhorate1003@gmail.com>
2026-01-08 12:13:25 +0530

    Commit from DESKTOP-VTQTMVO on 08-01-2026 at 121324.03

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Chavonn Spices – Pure • Authentic • Traditional</title>
+<title>Chav Onn Spices – Pure • Authentic • Traditional</title>
 
 <!-- Google Fonts -->
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&family=Playfair+Display:wght@700;800&display=swap" rel="stylesheet">
